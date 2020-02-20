0382f0e6385c883ce4ad5bec0169fe95ea6c07b2
Adarsh Menon <38039055+InfernoCoder11@users.noreply.github.com>
2020-02-20 15:55:37 +0530

    Main goal accomplished

--- main.py
+++ main.py
@@ -4,30 +4,45 @@ import os
 
 class earth():
     def __init__(self):
+        self.hdr = {'User-Agent': 'Mozilla/5.0'}
+
         self.script_dir = os.path.dirname(__file__)
 
         self.base_url = "https://earthview.withgoogle.com"
 
-    def main_page(self):
-        hdr = {'User-Agent': 'Mozilla/5.0'}
+        self.newpath = os.path.join(self.script_dir, "assets")
+        if not os.path.exists(self.newpath):
+            os.makedirs(self.newpath)
 
-        req = requests.get(self.base_url, headers = hdr)
+    def main_page(self):
+        req = requests.get(self.base_url, headers = self.hdr)
 
         soup = BeautifulSoup(req.content, "html.parser")
 
         self.next_link = soup.find("a", {"class": "button intro__explore"})["href"]
-
-        self.download(self.next_link)
     
     def download(self, link):
         if (link + ".jpg") not in os.listdir():
-            url = "https://www.gstatic.com/prettyearth/assets/full/" + link[-5:] + ".jpg"
+            url = "https://www.gstatic.com/prettyearth/assets/full/" + link.split("-")[-1] + ".jpg"
             r = requests.get(url, allow_redirects=True)
-            open(self.script_dir + "/" + link + ".jpg", 'wb').write(r.content)
+            open(self.newpath + link + ".jpg", 'wb').write(r.content)
 
-            print ("Downloaded " + link)
+            print ("Downloaded " + link[1:])
         else:
             print (link, "already exists. Skipping download.")
+    
+    def get_next_link(self):
+        req = requests.get(self.base_url + "/" + self.next_link, headers = self.hdr)
+
+        soup = BeautifulSoup(req.content, "html.parser")
+
+        self.next_link = soup.find("a", {"class": "pagination__link pagination__link--next"})["href"]
+
+    def main_loop(self):
+        while True:
+            self.download(self.next_link)
+            self.get_next_link()
 
 obj = earth()
 obj.main_page()
+obj.main_loop()
